dac44c15db37c7648a50261539253ffc988c8223
Nathan <119708133+nathantr97@users.noreply.github.com>
2022-12-09 12:46:41 -0800

    added array and loop to project

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+var topics =['HTML', 'CSS', 'Git', 'Javascript'];
+for(var x=0; x <topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*
+
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+
+} else if (topic === 'JaveScript') {
+    console.log("Let's study JaveScript!");
+
+} else {
+    console.log('Please try again!');
+}
+
+*/
